bfee0d70cb0be62adc21805d9c3b707cb20e609c
Sam Hokin <shokin@ncgr.org>
2018-04-23 14:33:31 -0600

    Coupla new toolz.

--- gff2bed.py
+++ gff2bed.py
@@ -1,38 +1,42 @@
 #!/usr/bin/python
 #
-# convert a GFF3 file to BED6 format, placing the attribute Name into the BED name column.
+# convert a GFF3 file to BED6 format, placing the desired attribute into the BED name column, or ID if that attribute is missing.
 
 import sys
 import datetime
 
-if len(sys.argv)!=2:
-    print "gff2bed.py <GFF3 file>"
+if len(sys.argv)!=3:
+    print "gff2bed.py <GFF3 file> <naming attribute>"
     sys.exit(0)
 
 filename = sys.argv[1]
+attribute = sys.argv[2].lower()
 
 f = open(filename, 'rU')
 
 for line in f:
-    chunks = line.split('\t')
-    chrom = chunks[0]
-    source = chunks[1]
-    feature = chunks[2]
-    start = int(chunks[3])
-    end = int(chunks[4])
-    score = chunks[5]
-    strand = chunks[6]
-    phase = chunks[7]
-    attributes = chunks[8]
-    pieces = attributes.split(';')
-    name = ""
-    for piece in pieces:
-        parts = piece.split('=')
-        if parts[0].lower()=='name':
-            name = parts[1]
-    if name!="":
-        print chrom+'\t'+str(start-1)+'\t'+str(end)+'\t'+name+'\t'+score+'\t'+strand
-    else:
-        print chrom+'\t'+str(start-1)+'\t'+str(end)+'\t'+"."+'\t'+score+'\t'+strand
+    if not line.startswith("#"):
+        chunks = line.split('\t')
+        chrom = chunks[0]
+        source = chunks[1]
+        feature = chunks[2]
+        start = int(chunks[3])
+        end = int(chunks[4])
+        score = chunks[5]
+        strand = chunks[6]
+        phase = chunks[7]
+        attributes = chunks[8]
+        pieces = attributes.split(';')
+	name = ""
+        for piece in pieces:
+            parts = piece.split('=')
+            if parts[0].lower()==attribute:
+                name = parts[1].rstrip()
+	    if parts[0].lower()=='id':
+		id = parts[1]
+        if name!="":
+            print chrom+'\t'+str(start-1)+'\t'+str(end)+'\t'+name+'\t'+score+'\t'+strand
+        else:
+            print chrom+'\t'+str(start-1)+'\t'+str(end)+'\t'+id+'\t'+score+'\t'+strand
 
 f.close()
--- mergefasta.py
+++ mergefasta.py
@@ -39,7 +39,7 @@ gff.write("##gff-version 3\n");
 # each input sequence is loaded as a SeqRecord and added to the merged sequence while outputting a GFF line
 input_handle = open(input_fasta, "rU")
 for record in SeqIO.parse(input_handle,"fasta") :
-##    merged_seq += separator_seq + record.seq
+    merged_seq += separator_seq + record.seq
     # GFF record
     start = end + separator_length + 1
     end = start + len(record.seq) - 1
@@ -50,10 +50,10 @@ input_handle.close()
 gff.close()
 
 # merged sequence record
-##merged_record = SeqRecord(merged_seq, id=accession, name="name here", description="description here")
+merged_record = SeqRecord(merged_seq, id=accession, name="name here", description="description here")
 
 # output the merged sequence
-##output_handle = open("merged.fasta", "w")
-##SeqIO.write(merged_record, output_handle, "fasta")
-##output_handle.close()
+output_handle = open("merged.fasta", "w")
+SeqIO.write(merged_record, output_handle, "fasta")
+output_handle.close()
 
